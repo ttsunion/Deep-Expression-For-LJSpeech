ad2fee8a576b8fc01da82b26e6c7789aaa4e8287
Fonzie <675688853@qq.com>
2018-01-28 15:36:59 +0800

    Update text.py

--- text.py
+++ text.py
@@ -7,6 +7,7 @@ str2num = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6,
  'y':25,'z':26,'%':27,',':28,'.':29,'?':30,
  "'":31,' ':32,'!':33}
 def text2label(text):
+    text = text + '%'
     label = [str2num[text[i].lower()] if text[i].lower() in str2num else 34 for i in range(len(text))]
     if len(label) <= pm.Tx:
         label += [0] * (pm.Tx - len(label))
